1a957c2758d2ca4223bedcf710e57f4e32671c63
Hemant Negi <nhemnt@gmail.com>
2021-12-19 23:54:27 +0530

    day 4 2021 part 1

---
+++ day4/4.js
@@ -0,0 +1,56 @@
+const { input, numbers } = require('./input');
+
+const normaliseValueIdMap = (bingo) => {
+    const obj = {}
+    let sum = 0;
+    bingo.forEach((row, i) => {
+        row.forEach((col, j) => {
+            obj[bingo[i][j]] = [i , j]
+            sum += Number(bingo[i][j])
+        })
+    })
+
+    return {obj, sum};
+}
+
+const bingoHashMap = []
+
+input.forEach((bingo,i) => {
+    const { obj, sum } = normaliseValueIdMap(bingo);
+    bingoHashMap.push({
+        valueIdMap: obj,
+        checked: {
+            row: [],
+            col : [],
+        },
+        sum
+    });
+
+})
+
+//part 1
+for(let i =0; i< numbers.length; i++ ){
+    const number = numbers[i];
+    for(let j =0; j<bingoHashMap.length; j++){
+        const bingo = bingoHashMap[j];
+        if(bingo.valueIdMap[number]){
+            if(bingo.checked.row[bingo.valueIdMap[number][0]]){
+                bingo.checked.row[bingo.valueIdMap[number][0]] = bingo.checked.row[bingo.valueIdMap[number][0]]+ 1;
+            }else {
+                bingo.checked.row[bingo.valueIdMap[number][0]] = 1;
+            }
+            if(bingo.checked.col[bingo.valueIdMap[number][1]]){
+                bingo.checked.col[bingo.valueIdMap[number][1]] = bingo.checked.col[bingo.valueIdMap[number][1]]+ 1;
+            }else {
+                bingo.checked.col[bingo.valueIdMap[number][1]] = 1;
+            }
+            bingo.sum -= Number(number);
+        }
+
+        //check win
+        if(i > 3 && (bingo.checked.row.includes(5) || bingo.checked.col.includes(5))){
+            console.log(Number(number) * Number(bingo.sum));
+            return;
+        }
+        }
+    }
