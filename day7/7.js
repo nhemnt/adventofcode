882d1474cdb690f917f31a2069c7e6df56cbdf8f
Hemant Negi <nhemnt@gmail.com>
2021-12-23 23:33:18 +0530

    day 7 part 2 2021

--- day7/7.js
+++ day7/7.js
@@ -11,3 +11,30 @@ for(let i = first; i<= last; i++){
 }
 
 console.log(Math.min(...Object.values(count)))
+
+
+//part 2
+const count2 = {}
+const sumCache = {}
+
+
+const calcSum = (j) => {
+    let sum = 0
+    for(let i = j; i > 0; i--){
+        if(sumCache[i]){
+            sum += sumCache[i];
+            break;
+        }
+        sum += i;
+    }
+    if(!sumCache[j]) sumCache[j] = sum
+    return sum;
+}
+const getSum = (i) => {
+    return sumCache[i] ? sumCache[i] : calcSum(i);
+}
+for(let i = first; i<= last; i++){
+    count2[i] = input.reduce((acc, current) => acc + getSum(Math.abs(current - i)), 0)
+}
+
+console.log(Math.min(...Object.values(count2)))
